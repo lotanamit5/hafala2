e394103ec1d48310e7cdbba1fb64ffb2fb874b4b
Lotan Amit <lotan.amit@gmail.com>
2022-05-22 08:47:57 +0000

    added hw2.c

---
+++ kernel/hw2.c
@@ -0,0 +1,56 @@
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+
+asmlinkage long sys_hello(void)
+{
+    printk("Hello, World!\n");
+    return 0;
+}
+
+asmlinkage int sys_set_status(int status)
+{
+    if (status != 0 && status != 1)
+    {
+        printk("Error: status must be 0 or 1\n");
+        return -EINVAL;
+    }
+
+    current->status = status;
+    return 0;
+}
+
+asmlinkage int sys_get_status(void)
+{
+    return current->status;
+}
+asmlinkage int sys_register_process(void)
+{
+    list_add_tail(&current->imp_entry, &current->imp_list);
+    return 0;
+}
+
+asmlinkage long sys_get_all_cs(void)
+{
+    struct list_head *ptr;
+    struct task_struct *entry;
+    long sum = 0;
+
+    if (list_empty(&current->imp_list))
+    {
+        printk("Error: no processes registered\n");
+        return -ENODATA;
+    }
+
+    for (ptr = current->imp_list.next; ptr != &current->imp_list; ptr = ptr->next) {
+        entry = list_entry(ptr, struct task_struct, imp_entry);
+        if(entry->status == 1)
+        {
+            sum += entry->tgid;
+        }
+    }
+
+    return sum;
+}
